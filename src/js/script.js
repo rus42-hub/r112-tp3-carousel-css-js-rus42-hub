df4efdadc4f1c29f3ab6d459ac963d67908111d2
rus42-hub <ruslana.budniak@edu.univ-fcomte.fr>
2025-11-18 16:56:51 +0100

    Carousel JS scroll based on dynamic item width

--- src/js/script.js
+++ src/js/script.js
@@ -6,20 +6,22 @@ const nextButton = document.querySelector(".carousel__button--next");
 
 // Largeur de défilement d’un item
 if (carousel) {
-    const defaultScroll = 260;
+    const premierItem = document.querySelector(".carousel__item");
+    const defaultScroll = premierItem.clientWidth;
+
 
   // Scroll au clic sur le bouton précédent
-  prevButton.addEventListener("click", () => {
+    prevButton.addEventListener("click", () => {
     carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
   });
 
   // Scroll au clic sur le bouton suivant
-  nextButton.addEventListener("click", () => {
+    nextButton.addEventListener("click", () => {
     carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });
   });
 
-  // (Опционально) Обновляем scrollAmount при ресайзе
-  window.addEventListener('resize', () => {
+  // (Optionel) Обновляем scrollAmount при ресайзе
+    window.addEventListener('resize', () => {
     const first = document.querySelector(".carousel__item");
     scrollAmount = first ? first.clientWidth : scrollAmount;
   });
